20863e8ccf6a1a7e989d6944ba5d0850c2777e62
Tiago-Stolf03 <tiagojs2007@gmail.com>
2026-04-01 13:42:02 -0300

    ok

---
+++ lista - exemplos e exercicios/exer5.py
@@ -0,0 +1,8 @@
+convidados = ["marcos","bruno","tiago","mateus","artur"]
+
+nome = input("informe seu nome: ")
+
+if nome in convidados:
+    print("Seja bem vindo!")
+else:
+    print("Você não foi convidado")
